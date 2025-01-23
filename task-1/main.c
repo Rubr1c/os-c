22ba04a16b0660a7688e15b11ec57667282ad001
Rubr1c <alizaghloul64@gamil.com>
2025-01-23 17:07:24 +0200

    added ls functionality

--- task-1/main.c
+++ task-1/main.c
@@ -2,56 +2,91 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <dirent.h>
 
 char path[256] = "root";
 
+void print_dirs() {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("./root");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+                printf("%s ", dir->d_name);
+            }
+        }
+        printf("\n\n");
+        closedir(d);
+    }
+}
+
 char **split_str(char *str, char del) {
     unsigned long size = strlen(str);
-    char ** out = malloc(sizeof(char *) * (size + 1));
+    char **out = malloc(sizeof(char *) * (size + 1));
     int idx = 0;
     int start = 0;
     for (int i = 0; i < size; i++) {
         if (str[i] == del) {
-           int len = i - start;
-           if (len > 0) {
-               out[idx] = malloc(len + 1);
-               memcpy(out[idx], str + start, len);
-               out[idx][len] = '\0'; 
-               idx++;
-           }
-           
+            int len = i - start;
+            if (len > 0) {
+                out[idx] = malloc(len + 1);
+                memcpy(out[idx], str + start, len);
+                out[idx][len] = '\0';
+                idx++;
+            }
             start = i + 1;
         }
     }
-
-    out[idx] = malloc(size - start);
-    strcpy(out[idx], str + start);
-    out[idx][size - start] = '\0'; 
-
-    out[idx + 1] = NULL;
+    int len = size - start;
+    if (len > 0) { 
+        out[idx] = malloc(len + 1);
+        memcpy(out[idx], str + start, len);
+        out[idx][len] = '\0';
+        idx++;
+    }
+    out[idx] = NULL;
     return out;
 }
 
-void handle_cmd(char** cmd) {
-    if (strcmp(cmd[0], "cd")) {
-        
+void handle_cmd(char **cmd) {
+    if (cmd[0] == NULL) return; 
+    if (strcmp(cmd[0], "cd") == 0) {
+    } else if (strcmp(cmd[0], "ls") == 0) {
+        print_dirs();
     }
 }
 
 int main() {
-
     while (1) {
-        char *cmd;
-        size_t size = 256;
-        size_t chars;
+        char *cmd = NULL;
+        size_t size = 0;
+        ssize_t chars;
 
-        cmd = malloc(size * sizeof(char));
         printf("user@%s> ", path);
         chars = getline(&cmd, &size, stdin);
+        if (chars == -1) {
+            free(cmd);
+            break; 
+        }
+        if (chars > 0 && cmd[chars - 1] == '\n') {
+            cmd[chars - 1] = '\0';
+        }
         char **tokens = split_str(cmd, ' ');
-        if (strcmp(tokens[0], "quit")) {
+        if (strcmp(tokens[0], "quit") == 0) {
+            free(cmd);
+            for (int i = 0; tokens[i] != NULL; i++) {
+                free(tokens[i]);
+            }
+            free(tokens);
             break;
         }
         handle_cmd(tokens);
+        free(cmd);
+        for (int i = 0; tokens[i] != NULL; i++) {
+            free(tokens[i]);
+        }
+        free(tokens);
     }
+    return 0;
 }
